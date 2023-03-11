094be3ffeecd48aada1df3f98040c0789be1807c
Mudana-Grune <mudana@gruneasia.info>
2023-03-11 11:13:44 +0800

    update floating button

--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,27 @@ import Cookies from 'js-cookie';
 import Header from '@/components/Header';
 import AboutUs from '@/components/AboutUs';
 import WhyUs from '@/components/WhyUs';
-import { FloatingWhatsApp } from 'react-floating-whatsapp'
-import { APP_NAME, WA_NUM } from '@/utils/constants';
+import { APP_NAME, WA_NUM, WA_URL } from '@/utils/constants';
+import { FloatingButton } from '@/components/WhatsApp/FloatingButton';
 
 
 const Home = () => {
   const { t } = useTranslation('common');
   const router = useRouter();
   const [waOpen, setWaOpen] = useState(false);
+
+
+  const composeMessage = (v) => {
+    const header = `*##${APP_NAME}*`;
+    const fullMessage = `${header}\n\n${v}`;
+    return encodeURIComponent(fullMessage);
+  }
+  function handleSubmit(e, value) {
+    e.preventDefault();
+    const message = composeMessage(value);
+    const uri = `${WA_URL}?text=${message}`;
+    window.open(uri, '_blank').focus()
+  }
 
   useEffect(() => {
     const locale = Cookies.get('rebality-lang') || 'en';
@@ -45,28 +58,29 @@ const Home = () => {
             </div>
           </div>
         </section>
+        <FloatingButton
+          accountName={`${APP_NAME} - Admin`}
+          phoneNumber={WA_NUM}
+          darkMode={true}
+          notification={true}
+          notificationLoop={1}
+          notificationDelay={20}
+          notificationSound={false}
+          allowClickAway={true}
+          allowEsc={true}
+          statusMessage={t('wa_status')}
+          placeholder={t('wa_placeholder')}
+          chatMessage={t('wa_chat')}
+          avatar={'/assets/images/logo.png'}
+          buttonStyle={{ left: '2rem' }}
+          chatboxClassName={`${waOpen ? 'open' : 'close'} chatbox`}
+          onClick={() => setWaOpen(true)}
+          onClose={() => setWaOpen(false)}
+          onSubmit={handleSubmit}
+        />
         <ContactUs />
         <Map />
       </BaseLayout>
-      <FloatingWhatsApp
-        accountName={`${APP_NAME} - Admin`}
-        phoneNumber={WA_NUM}
-        darkMode={true}
-        notification={true}
-        notificationLoop={1}
-        notificationDelay={20}
-        notificationSound={false}
-        allowClickAway={true}
-        allowEsc={true}
-        statusMessage={t('wa_status')}
-        placeholder={t('wa_placeholder')}
-        chatMessage={t('wa_chat')}
-        avatar={'/assets/images/logo.png'}
-        buttonStyle={{ left: '2rem' }}
-        chatboxClassName={`${waOpen ? 'open' : 'close'} chatbox`}
-        onClick={() => setWaOpen(true)}
-        onClose={() => setWaOpen(false)}
-      />
     </>
   )
 }
